e1ef358c7aa4bd9a6af90fb7cbe07b34b2311f2a
Akshai Kumar <johanliebert1205@gmail.com>
2024-09-06 21:46:03 +0530

    added image carousel

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -18,25 +18,24 @@ const ImageCarousel = () => {
         <div className="w-full mx-auto my-8 mb-6">
             <Slider {...settings}>
                 <div>
-
                     <img
                         src="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1692947383286-714WUJlhbLS._SL1500_.jpg"
                         alt="Slide 1"
-                        className="w-full h-64 object-contain rounded-lg shadow-md"
+                        className="w-full h-64 object-contain rounded-lg shadow-md transform transition duration-300 hover:scale-105 hover:shadow-lg"
                     />
                 </div>
                 <div>
                     <img
                         src="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1692255251854-xbox.jpg"
                         alt="Slide 2"
-                        className="w-full h-64 object-contain rounded-lg shadow-md"
+                        className="w-full h-64 object-contain rounded-lg shadow-md transform transition duration-300 hover:scale-105 hover:shadow-lg"
                     />
                 </div>
                 <div>
                     <img
                         src="https://storage.googleapis.com/fir-auth-1c3bc.appspot.com/1692257709689-logitech%20heaphone.jpg"
                         alt="Slide 3"
-                        className="w-full h-64 object-contain rounded-lg shadow-md"
+                        className="w-full h-64 object-contain rounded-lg shadow-md transform transition duration-300 hover:scale-105 hover:shadow-lg"
                     />
                 </div>
             </Slider>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,6 @@ import ToggleMode from "./ToggleMode";
 
 const Navbar = () => {
     const { data: categories } = useProducts("products/category");
-    console.log(categories)
     const dispatch = useDispatch();
     const [selectedCategory, setSelectedCategory] = useState("");
 
@@ -19,41 +18,45 @@ const Navbar = () => {
     return (
         <nav className="bg-gray-900 p-4 shadow-md">
             <div className="container mx-auto flex justify-between items-center">
+
                 <div className="text-white text-2xl font-bold">
                     <Link to="/" className="hover:text-gray-400 transition duration-300">
                         E-Shop
                     </Link>
                 </div>
+
+
                 <div className="flex items-center space-x-4">
+                    <button
+                        onClick={() => handleCategoryClick("")}
+                        className={`py-2.5 px-4 rounded-md text-white text-xl font-medium transition duration-300 
+                            ${selectedCategory === "" ? 'bg-gray-700' : 'bg-gray-600 hover:bg-gray-500'} 
+                            focus:outline-none focus:ring-2 focus:ring-gray-400`}
+                    >
+                        All
+                    </button>
+                    {categories.map((category, index) => (
+                        <button
+                            key={index}
+                            onClick={() => handleCategoryClick(category)}
+                            className={`py-2.5 px-4 rounded-md text-white text-base font-medium transition duration-300 
+                                ${selectedCategory === category ? 'bg-gray-700' : 'bg-gray-600 hover:bg-gray-500'} 
+                                focus:outline-none focus:ring-2 focus:ring-gray-400`}
+                        >
+                            {category.toUpperCase()}
+                        </button>
+                    ))}
+                </div>
+
+
+                <div className="flex items-center space-x-3">
                     <Link to="favorites" className="text-white text-lg font-medium hover:text-gray-400 transition duration-300">
                         Favorites
                     </Link>
-                    <Link to="/cart" className="text-yellow-400 font-semibold">
+                    <ToggleMode />
+                    <Link to="/cart" className="text-yellow-400 mr-2 text-lg font-semibold">
                         🛒Cart
                     </Link>
-                    <ToggleMode />
-                    <div className="flex space-x-4">
-                        <button
-
-                            onClick={() => handleCategoryClick("")}
-                            className={`py-2 px-4 rounded-md text-white text-lg font-medium transition duration-300 
-                                    ${selectedCategory === "" ? 'bg-gray-700' : 'bg-gray-600 hover:bg-gray-500'} 
-                                    focus:outline-none focus:ring-2 focus:ring-gray-400`}
-                        >
-                            All
-                        </button>
-                        {categories.map((category, index) => (
-                            <button
-                                key={index}
-                                onClick={() => handleCategoryClick(category)}
-                                className={`py-2 px-4 rounded-md text-white text-lg font-medium transition duration-300 
-                                    ${selectedCategory === category ? 'bg-gray-700' : 'bg-gray-600 hover:bg-gray-500'} 
-                                    focus:outline-none focus:ring-2 focus:ring-gray-400`}
-                            >
-                                {category.toUpperCase()}
-                            </button>
-                        ))}
-                    </div>
                 </div>
             </div>
         </nav>
